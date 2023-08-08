6230ba12840d01647d5b3443ce6700b81204842d
Jayavani9 <51267675+Jayavani9@users.noreply.github.com>
2023-08-08 15:01:25 -0500

    Update InvertBinaryTree.java

--- InvertBinaryTree.java
+++ InvertBinaryTree.java
@@ -10,4 +10,31 @@ Output: [2,3,1]
 Input: root = []
 Output: []
 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null) return null;
 
+        TreeNode t = root.right;
+        root.right = root.left;
+        root.left = t;
+        invertTree(root.right);
+        invertTree(root.left);
+
+        return root;
+    }
+}
