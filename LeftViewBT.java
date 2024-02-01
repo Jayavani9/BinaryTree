89b3451c0b70532e6862ced59a620ae428b6a14a
Jayavani9 <51267675+Jayavani9@users.noreply.github.com>
2024-02-01 12:11:42 -0800

    Update LeftViewBT.java

--- LeftViewBT.java
+++ LeftViewBT.java
@@ -61,3 +61,26 @@ class Solution {
         dfs(root.right, level+1);
     }
 }
+
+
+//Other solution using DFS
+class Solution {
+    List<Integer> res;
+    public List<Integer> rightSideView(TreeNode root) {
+         res = new ArrayList<>();
+         if(root == null) return res;
+         dfs(root, 0);
+         return res;
+    }
+
+    private void dfs(TreeNode root, int level)
+    {
+        if(root == null) return;
+        if(res.size() == level) {
+            res.add(root.val);
+        }
+       
+        dfs(root.right, level+1);
+        dfs(root.left, level+1);
+    }
+}
