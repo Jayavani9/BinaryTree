096d0b2287fe986936669fd9ce94c598043179e8
Jayavani Akkiraju <51267675+Jayavani9@users.noreply.github.com>
2024-06-25 14:14:50 -0700

    Update LeftViewBT.java

--- LeftViewBT.java
+++ LeftViewBT.java
@@ -1,86 +1,27 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+//https://www.geeksforgeeks.org/problems/left-view-of-binary-tree/1?page=1&company%5B%5D=Amazon&category%5B%5D=Tree&sortBy=
 
-//Using BFS 
-class Solution {
-    public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> res = new ArrayList<>();
-        if(root == null) return res;
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        while(!q.isEmpty() )
-        {
-            int size = q.size();
-            for(int i = 0; i < size; i++)
-            {
-                TreeNode cur = q.poll();
-                if(i == 0) res.add(cur.val);
-                if(cur.left != null) q.add(cur.left);
-                if(cur.right != null) q.add(cur.right);
-            }
-        }
-
-        return res;
-    }
-}
-
-
-
-//Solution using DFS:
-
-class Solution {
-    List<Integer> res;
-    public List<Integer> rightSideView(TreeNode root) {
-         res = new ArrayList<>();
-         if(root == null) return res;
-         dfs(root, 0);
-         return res;
-    }
-
-    private void dfs(TreeNode root, int level)
+class Tree
+{
+    //Function to return list containing elements of left view of binary tree.
+    ArrayList<Integer> leftView(Node root)
     {
-        if(root == null) return;
-        if(res.size() == level) {
-            res.add(root.val);
-        }
-
-        dfs(root.left, level+1);
-        dfs(root.right, level+1);
-    }
-}
-
-
-//Other solution using DFS
-class Solution {
-    List<Integer> res;
-    public List<Integer> rightSideView(TreeNode root) {
-         res = new ArrayList<>();
-         if(root == null) return res;
-         dfs(root, 0);
-         return res;
-    }
-
-    private void dfs(TreeNode root, int level)
-    {
-        if(root == null) return;
-        if(res.size() == level) {
-            res.add(root.val);
-        }
-       
-        dfs(root.right, level+1);
-        dfs(root.left, level+1);
+      // Your code here
+        //Tc: O(n) Sc: O(n)
+       ArrayList<Integer> ans = new  ArrayList<Integer>();
+       Queue<Node> q = new LinkedList<>();
+       if(root == null) return ans;
+       q.add(root);
+       while(!q.isEmpty())
+       {
+          int size = q.size();
+          for(int i = 0; i < size; i++)
+          {
+              Node cur = q.poll();
+              if(i == 0) ans.add(cur.data);
+              if(cur.left != null)q.add(cur.left);
+              if(cur.right != null)q.add(cur.right);
+          }
+       }
+       return ans;
     }
 }
