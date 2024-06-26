620e573bad36ba720fcdc5d82705a1d4a71a393e
Jayavani Akkiraju <51267675+Jayavani9@users.noreply.github.com>
2024-06-25 20:23:34 -0700

    Update kthLargeEle.java

--- kthLargeEle.java
+++ kthLargeEle.java
@@ -1,5 +1,43 @@
 //https://www.geeksforgeeks.org/problems/kth-largest-element-in-bst/1?page=2&company%5B%5D=Amazon&category%5B%5D=Tree&sortBy=
 
+
+class Solution
+{
+    // return the Kth largest element in the given BST rooted at 'root'
+    public int kthLargest(Node root,int K)
+    {
+        //Your code here
+        //Tc: O(n) Sc: O(n)
+        
+        Stack<Node> stack = new Stack<>();
+        Node current = root;
+        int count = 0;
+        
+        // Reverse in-order traversal using a stack
+        while (current != null || !stack.isEmpty()) {
+            // Push all right children to stack
+            while (current != null) {
+                stack.push(current);
+                current = current.right;
+            }
+
+            // Process the node
+            current = stack.pop();
+            count++;
+            if (count == K) {
+                return current.data;  // Found kth largest
+            }
+
+            // Move to the left child
+            current = current.left;
+        }
+
+        return -1; 
+    }
+}
+
+
+
 class Solution
 {
     int res = -1;
